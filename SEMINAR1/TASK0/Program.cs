21e30caeee1f26f1f88c13e387bacd71c46d7419
Jack <Ev.ydu@yandex.ru>
2022-11-27 16:15:35 +0400

    added tasks 0 1 2 3

---
+++ SEMINAR1/TASK0/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите первое число:");
+string? answer1 = Console.ReadLine();
+int number1 = Convert.ToInt32(answer1);
+
+Console.WriteLine("Введите второе число:");
+string? answer2 = Console.ReadLine();
+int number2 = Convert.ToInt32(answer2);
+
+if(number2 * number2 == number1)
+{
+    Console.WriteLine($"{number2} * {number2} = {number1}");
+}
+else
+{
+    Console.WriteLine("НЕ ТАК");
+}
---
+++ SEMINAR1/TASK3/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите трехзначное число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number >= 100 && number < 1000)
+{
+    int mod = number % 10;
+    Console.WriteLine(mod);
+}
+else
+{
+    Console.WriteLine("Введите трехзначное число!");
+}
