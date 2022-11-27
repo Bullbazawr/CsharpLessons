882bdc121da7be3fa4b63a7507bae1ce7da49678
Jack <Ev.ydu@yandex.ru>
2022-11-27 17:15:19 +0400

    Completed the first task from home work

---
+++ SEMINAR1/HomeWorkTask2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Ввелите число A:");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Ввелите число B:");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+if (numberA == numberB) Console.WriteLine("Числа равны");
+
+if (numberA > numberB) Console.WriteLine("A=Max, B=Min");
+
+if (numberA < numberB) Console.WriteLine("B=Max, A=Min");
