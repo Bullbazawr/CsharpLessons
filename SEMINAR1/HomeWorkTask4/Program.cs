2bee9282cf5ee9c8a3580528638996ec9457fcb6
Jack <Ev.ydu@yandex.ru>
2022-11-28 13:21:56 +0400

    completed task4 from homework

---
+++ SEMINAR1/HomeWorkTask4/Program.cs
@@ -0,0 +1,37 @@
+﻿Console.WriteLine("Введите число A:");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число B:");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число C:");
+int numberC = Convert.ToInt32(Console.ReadLine());
+
+if (numberA & numberB == numberC) Console.WriteLine("Все числа равны и являются максимальным значением");
+
+if (numberA > numberB)
+    if (numberA > numberC) Console.WriteLine($"Max={numberA}");
+    else if (numberA < numberC)
+    {
+        Console.WriteLine($"Max={numberC}");
+    }
+    else
+    {
+        Console.WriteLine($"{numberA} и {numberC} равны и являются максимальным значением");
+    }
+else if (numberA < numberB)
+     if (numberB > numberC) Console.WriteLine($"Max={numberB}");
+     else if (numberB < numberC)
+     {
+        Console.WriteLine ($"Max={numberC}");
+     }
+     else
+     {
+        Console.WriteLine($"{numberB} и {numberC} равны и являются максимальным значением");
+     }
+else if (numberA == numberB)
+    if (numberA > numberC) Console.WriteLine($"{numberA} и {numberB} равны и являются максимальным значением");
+    else
+    {
+        Console.WriteLine($"Max= {numberC} ");
+    }
