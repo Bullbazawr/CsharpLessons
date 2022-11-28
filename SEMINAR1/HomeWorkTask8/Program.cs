2373b7b2666a8440cd4b2af3aed6ee3006e0b2f0
Jack <Ev.ydu@yandex.ru>
2022-11-28 13:50:43 +0400

    completed task8 from homework

---
+++ SEMINAR1/HomeWorkTask8/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+for (int x = 1; x <= number; x += 1)
+    if ((x % 2) == 0)
+{
+    Console.WriteLine(x + " ");
+}
