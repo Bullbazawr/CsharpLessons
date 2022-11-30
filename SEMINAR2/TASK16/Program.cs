0e6859f786e0c1700a8dbee61dc8ae20f7b08b22
Jack <Ev.ydu@yandex.ru>
2022-11-30 13:54:16 +0400

    completed all the tasks from the seminar two

---
+++ SEMINAR2/TASK16/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите первое число:");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число:");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+int x = number1 * number1;
+int y = number2 * number2;
+
+if (number1 == y || number2 == x )
+    {
+        Console.WriteLine("Верно");
+    }
+else
+    {
+        Console.WriteLine("неверно");
+    }    
