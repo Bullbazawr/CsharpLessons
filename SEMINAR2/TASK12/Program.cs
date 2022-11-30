d761824ccc8830d385d4b21392b0e9ca3f0baf27
Jack <Ev.ydu@yandex.ru>
2022-11-30 13:57:00 +0400

    completed all the tasks from the seminar two

---
+++ SEMINAR2/TASK12/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите первое число:");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine ("Введите второе число");
+int number2= Convert.ToInt32(Console.ReadLine());
+
+if (number2 % number1 == 0)
+   {
+    Console.WriteLine($" {number2} кратно {number1} ");
+   }
+else
+   {
+    Console.WriteLine($"Не кратно! остаток = {number2 % number1} ");
+   } 
---
+++ SEMINAR2/TASK14/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int remainder7 = number % 7;
+int remainder23 = number % 23;
+
+if (remainder7 == 0 && remainder23 == 0)
+    {
+        Console.WriteLine("Верно");
+    }
+else
+    {
+        Console.WriteLine("Неверно");
+    }    
